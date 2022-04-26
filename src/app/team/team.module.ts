bb0cf0508faaabca8663cbb43f83a90488c3eb0d
jodu07 <jhonnat.dussan@gmail.com>
2022-04-26 02:34:30 -0500

    initial structure

---
+++ src/app/team/team.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TeamPageComponent } from './pages/team-page/team-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    TeamPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TeamModule { }
